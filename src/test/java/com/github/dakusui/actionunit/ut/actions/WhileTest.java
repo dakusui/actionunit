3dc77365868b0df8a56ce292cb59842cbd17d4b9
Hiroshi Ukai <dakusui@gmail.com>
2022-10-10 17:10:54 +0900

    Issue-#68: Add tests for 'while' action

--- src/test/java/com/github/dakusui/actionunit/ut/actions/WhileTest.java
+++ src/test/java/com/github/dakusui/actionunit/ut/actions/WhileTest.java
@@ -7,21 +7,39 @@ import com.github.dakusui.actionunit.io.Writer;
 import com.github.dakusui.actionunit.ut.utils.TestUtils;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Predicate;
 
+import static com.github.dakusui.pcond.TestAssertions.assertThat;
 import static com.github.dakusui.pcond.forms.Printables.predicate;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.function.Predicate.isEqual;
 
-public class WhileTest {
+public class WhileTest extends TestUtils.TestBase {
   @Test
   public void givenWhileAction_whenPerform_thenRepeatsWhileConditionIsTrue() {
-    Action whileAction = ActionSupport.repeatWhile(iIsLessThan10()).perform(printAndIncrementI()).build();
+    List<String> out = new LinkedList<>();
+    Action whileAction = ActionSupport.repeatWhile(iIsLessThan10()).perform(printAndIncrementI(out)).build();
     TestUtils.createReportingActionPerformer().performAndReport(whileAction, Writer.Std.OUT);
+    assertThat(out, isEqual(asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")));
   }
 
-  private static Action printAndIncrementI() {
+  @Test
+  public void givenWhileActionWithConditionNeverMet_whenPerform_thenNothingHappens() {
+    List<String> out = new LinkedList<>();
+    Action whileAction = ActionSupport.repeatWhile(iIsGreaterThan0()).perform(printAndIncrementI(out)).build();
+    TestUtils.createReportingActionPerformer().performAndReport(whileAction, Writer.Std.OUT);
+    assertThat(out, isEqual(emptyList()));
+  }
+
+
+  private static Action printAndIncrementI(List<String> out) {
     return ActionSupport.simple("print:i++", c -> {
       int i = c.defined("i") ? c.valueOf("i") : 0;
       System.out.println(i);
+      out.add(Integer.toString(i));
       i++;
       c.assignTo("i", i);
     });
@@ -31,6 +49,13 @@ public class WhileTest {
     return predicate("i is less than 10", (Context context) -> {
       int i = context.defined("i") ? context.valueOf("i") : 0;
       return i < 10;
+    });
+  }
+
+  private static Predicate<Context> iIsGreaterThan0() {
+    return predicate("i is greater than 0", (Context context) -> {
+      int i = context.defined("i") ? context.valueOf("i") : 0;
+      return i > 0;
     });
   }
 }
