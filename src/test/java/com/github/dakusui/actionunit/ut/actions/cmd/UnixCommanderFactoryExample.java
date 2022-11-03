a4a18145b152e6c2de538397d544cc83a87827c6
Hiroshi Ukai <dakusui@gmail.com>
2022-11-03 09:02:45 +0900

    Issue-#102,#103,#104: Clean up UnixCommanderFactoryManager and its Test.

--- src/test/java/com/github/dakusui/actionunit/ut/actions/cmd/UnixCommanderFactoryExample.java
+++ src/test/java/com/github/dakusui/actionunit/ut/actions/cmd/UnixCommanderFactoryExample.java
@@ -2,7 +2,6 @@ package com.github.dakusui.actionunit.ut.actions.cmd;
 
 import com.github.dakusui.actionunit.actions.cmd.ShellManager;
 import com.github.dakusui.actionunit.actions.cmd.UnixCommanderFactory;
-import com.github.dakusui.actionunit.actions.cmd.UnixCommanderFactoryManager;
 import com.github.dakusui.actionunit.actions.cmd.unix.SshOptions;
 import com.github.dakusui.actionunit.core.Context;
 import org.junit.Test;
